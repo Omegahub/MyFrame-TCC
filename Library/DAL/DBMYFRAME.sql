ca99eabcf7b7942e2f0ff83a0dcf524f34871494
Marcelo-Alvess <marceloalvesdesousa23do1@gmail.com>
2020-02-23 19:01:21 -0300

    Alteração no Banco de Dados

--- Library/DAL/DBMYFRAME.sql
+++ Library/DAL/DBMYFRAME.sql
@@ -1,93 +1,148 @@
 drop database DBMYFRAME
-
+GO
 create database DBMYFRAME
-
+GO
 use DBMYFRAME
+GO
 
-create table Gestor (
-	idGestor     int not null identity(1,1) primary key,
-	nomeGestor	 varchar(100),
-	dtNascGestor date
+create table TGESTOR (
+	GEIDGESTOR      int not null identity(1,1) primary key,
+	GENMGESTOR 	    varchar(100),
+	GEDTNASCGESTOR  date
 );
+GO
 
-create table Turma (
-	idTurma   int not null identity(1,1) primary key,
-	nomeCurso varchar(50)
+create table TTURMA (
+	TUIDTURMA   int not null identity(1,1) primary key,
+	TUNMTURMA   varchar(50)
 );
+GO
 
-create table AvaliacaoCriterio (
-	idCriterioAv int not null identity(1,1) primary key,
-	dsCriterioAv int,
+create table TAVALIACAOCRITERIO (
+	AVCRIDAVCR   int not null identity(1,1) primary key,
+	AVCRDESCAVCR int,
 );
+GO
 
-create table ItensTurma (
-	idItemTurma int not null identity(1,1) primary key,
-	descItem	varchar(100),
+create table TITENSTURMA (
+	ITTUIDITTU   int not null identity(1,1) primary key,
+	ITTUDESCITTU varchar(100),
 );
+GO
 
-create table Evento (
-	idEvento		int not null identity(1,1) primary key,
-	nomeEvento		varchar(50),
-	endEvento		varchar(200),
-	dtEvento		date,
-	descEvento		varchar(200),
-	capaciMaxEvento varchar(50),
+create table TEVENTO (
+	EVIDEVENTO		int not null identity(1,1) primary key,
+	EVNMEVENTO		varchar(50),
+	EVENDEVENTO		varchar(200),
+	EVDTEVENTO		date,
+	EVDESCEVENTO	varchar(200),
+	EVCAPAMAXEVENTO varchar(50),
 
-	gestorIdGestor  int constraint fk_Gestor_idGestor foreign key(gestorIdGestor) references Gestor(idGestor)
+	GEIDGESTOR  int constraint EV_IDGESTOR_FK foreign key(GEIDGESTOR) references TGESTOR(GEIDGESTOR)
 );
+GO
 
-
-CREATE TABLE TEVENTO
-(
-	EVIDEVENTO
-	EVNOMEEVENTO
-)
-
-create table Visitante (
-	idVisitante	   int not null identity(1,1) primary key,
-	nomeVisitante  varchar(100),
-	cpfVisitante   varchar(15),
-	cidaVisitante  varchar(200),
-	emailVisitante varchar(50),
-	nascVisitante  date,
-
-	eventoIdEvento int constraint fk_Evento_idEvento foreign key(eventoIdEvento) references Evento(idEvento)
+create table TVISITANTE (
+	VIIDVISI	   int not null identity(1,1) primary key,
+	VINMVISI       varchar(100),
+	VICPFVISI      varchar(15),
+	VICIDAVISI     varchar(200),
+	VIEMAILVISI    varchar(50),
+	VINASCVISI     date,
+	
+	EVIDEVENTO int constraint VISI_IDEVENTO_FK foreign key(EVIDEVENTO) references TEVENTO(EVIDEVENTO)
 );
+GO
 
-create table Projeto (
-	idProjeto	 int not null identity(1,1) primary key,
-	nomeProjeto	 varchar(100),
-	localProjeto varchar(100),
-	dtProjeto	 date,
-	descProjeto  varchar(100),
+create table TPROJETO (
+	PROIDPROJ	 int not null identity(1,1) primary key,
+	PRONMPROJ	 varchar(100),
+	PROLOCPROJ   varchar(100),
+	PRODTPROJ	 date,
+	PRODESCPROJ  varchar(100),
 
-	eventoIdEvento int constraint fk2_Evento_idEvento foreign key(eventoIdEvento) references Evento(idEvento)
+	EVIDEVENTO int constraint PROJ_IDEVENTO_FK foreign key(EVIDEVENTO) references TEVENTO(EVIDEVENTO)
 );
+GO
 
 CREATE TABLE TAVALIACAO 
 (
-	AVIDVISI		INT			NOT NULL,
-	AVIDCRITAVA		INT			NOT NULL,
-	AVIDPROJ		INT			NOT NULL,
-	AVNOTAAVA		VARCHAR(2)	NOT NULL,
-	AVDATAAVA		DATETIME,
-	AVFEEDBACKAVA	VARCHAR(500)
+	AVIDVISI	  int,
+	AVIDCRITAVA	  int,
+	AVIDPROJ	  int,
+	AVNOTAAVA	  varchar(20),
+	AVDATAAVA	  date,
+	AVFEEDBACKAVA varchar(500)
 
-	CONSTRAINT AV_PROJ_FK PRIMARY KEY (AVIDCRITAVA, AVIDPROJ, AVIDVISI)
+	 constraint AV_PROJ_FK primary key (AVIDCRITAVA, AVIDPROJ, AVIDVISI) 
+);
+GO
+
+create table TPROJCRIAVA (
+	PCAIDPROJ int constraint PCA_IDPROJ_FK	foreign key(PCAIDPROJ)  references TPROJETO(PROIDPROJ),
+	PCAIDCRAV int constraint PCA_IDCRAV_FK  foreign key(PCAIDCRAV)  references TAVALIACAOCRITERIO(AVCRIDAVCR)
+);
+GO
+
+create table TTURITETUR (
+	TITIDTURMA int constraint TIT_IDTURMA_FK  foreign key(TITIDTURMA)  references TTURMA(TUIDTURMA),
+	TITIDITTU  int constraint TIT_IDITTU_FK   foreign key(TITIDITTU)   references TITENSTURMA(ITTUIDITTU)
+);
+GO
+
+create table TPROJTUR (
+	PTIDPROJ  int constraint PT_IDPROJ_FK   foreign key(PTIDPROJ)   references TPROJETO(PROIDPROJ),
+	PTIDTURMA int constraint PT_IDTURMA_FK  foreign key(PTIDTURMA)	references TTURMA(TUIDTURMA)
 );
 
-create table Projeto_CriterioAvaliacao (
-	projetoIdProjeto	   int constraint fk_Projeto_idProjeto		 foreign key(projetoIdProjeto)		 references Projeto(idProjeto),
-	criterioAvIdCriterioAv int constraint fk_CriterioAv_idCriterioAv foreign key(criterioAvIdCriterioAv) references CriterioAvaliacao(idCriterioAv)
-);
+/*INSERT'S*/
 
-create table Turma_ItemTurma (
-	turmaIdTurma		 int constraint fk_Turma_idTurma		 foreign key(turmaIdTurma)		   references Turma(idTurma),
-	itemTurmaIdItemTurma int constraint fk_ItemTurma_idItemTurma foreign key(itemTurmaIdItemTurma) references ItensTurma(idItemTurma)
-);
+/*GESTOR*/
+insert into TGESTOR 
+	values('gestor','2020-01-01'),
+		  ('gestor2','2020-01-02')
 
-create table Projeto_Turma (
-	projetoIdProjeto int constraint fk2_Projeto_idProjeto foreign key(projetoIdProjeto) references Projeto(idProjeto),
-	turmaIdTurma	 int constraint fk2_Turma_idTurma	 foreign key(turmaIdTurma)	   references Turma(idTurma)
-);
+select * from TGESTOR
 
+/*TURMA*/
+insert into TTURMA 
+	values('turma'),
+		  ('turma2')
+
+select * from TTURMA
+
+/*AVALIACAOCRITERIO*/
+insert into TAVALIACAOCRITERIO
+	values(21),
+		  (22)
+
+select * from TAVALIACAOCRITERIO
+
+/*ITENSTURMA*/
+insert into TITENSTURMA
+	values('itensturma'),
+		  ('itensturma2')
+
+select * from TITENSTURMA
+
+/*EVENTO*/
+insert into TEVENTO (EVNMEVENTO, EVENDEVENTO, EVDTEVENTO, EVDESCEVENTO, EVCAPAMAXEVENTO)
+	values('evento','2 evento','2020-01-01','3 evento','4 evento'),
+		  ('evento2','5 evento','2020-01-02','6 evento','7 evento')
+
+select * from TEVENTO
+drop TABLE TAVALIACAO
+
+/*VISITANTE*/
+insert into TVISITANTE (VINMVISI,VICPFVISI,VICIDAVISI,VIEMAILVISI,VINASCVISI)
+	values('visitante','2 visitante','3 visitante','4 visitante','2020-01-01'),
+		  ('visitante2','5 visitante','6 visitante','7 visitante','2020-01-02')
+
+select * from TVISITANTE
+
+/*AVALIACAO*/
+insert into TAVALIACAO (AVIDVISI, AVIDCRITAVA, AVIDPROJ, AVNOTAAVA, AVDATAAVA, AVFEEDBACKAVA)
+	values(1,1,1,'avaliacao','2020-01-01','2 avaliacao'),
+		  (2,2,2,'valiacao2','2020-01-02', '3 avaliacao')
+
+select * from TAVALIACAO
